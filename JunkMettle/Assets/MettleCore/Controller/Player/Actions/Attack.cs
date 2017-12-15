156341d5e4075effb5911530023613dcca3cd2aa
dpoole66 <donpoole66@gmail.com>
2017-12-15 14:16:10 -0800

    added attack button to player

---
+++ JunkMettle/Assets/MettleCore/Controller/Player/Actions/Attack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.AI;
+
+
+public class Attack : MonoBehaviour {
+
+
+	private Animator thisAnimator;
+
+	int AttackHash = Animator.StringToHash("Attacking");
+
+	public void Start(){
+
+		thisAnimator = GetComponentInChildren<Animator> ();
+
+	}
+
+	public void AttackNow (){
+
+		thisAnimator.SetTrigger (AttackHash);
+
+	}
+}
--- JunkMettle/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ JunkMettle/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -10,8 +10,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
     public class ThirdPersonUserControl : MonoBehaviour
     {
 		private ThirdPersonCharacter playerCharacter; // A reference to the ThirdPersonCharacter on the object
-        private Transform m_Cam;                  // A reference to the main camera in the scenes transform
-        private Vector3 m_CamForward;             // The current forward direction of the camera
+      //  private Transform m_Cam;                  // A reference to the main camera in the scenes transform
+      //  private Vector3 m_CamForward;             // The current forward direction of the camera
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
 		public NavMeshAgent playerAgent;
@@ -19,7 +19,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         
         private void Start()	{
 
-
+			/*
             // get the transform of the main camera
             if (Camera.main != null)
             {
@@ -31,7 +31,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
                     "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
                 // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
             }
-
+			*/
 
             // get the third person character ( this should never be null due to require component )
 			playerCharacter = GetComponent<ThirdPersonCharacter>();
